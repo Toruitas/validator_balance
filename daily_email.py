881cb76959db1b2110652cd1bb193cffa78ab704
Stuart Leitch <Toruitas@gmail.com>
2020-12-19 10:04:38 +0000

    Add updates even if no file present.

--- daily_email.py
+++ daily_email.py
@@ -8,12 +8,7 @@ from sendgrid.helpers.mail import (Mail, Attachment, FileContent, FileName, File
 to_email = os.environ.get('TO_EMAIL')
 sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
 
-message = Mail(
-    from_email=to_email,
-    to_emails=to_email,
-    subject="Staker status is UP. Yesterday's CSV attached.",
-    html_content='See CSV for earnings.'
-)
+
 
 now_utc = datetime.now(timezone.utc)
 today = now_utc.date()
@@ -23,19 +18,30 @@ try:
     with open(f'csvs/daily/{yesterday}.csv', 'rb') as f:
         data = f.read()
         f.close()
+    attach_file = True
+    body_content = f"The staking server is up, and there is 1 attachment for today, {today}."
 except FileNotFoundError as e:
-    print(f"Yesterday's file 'csvs/daily/{yesterday}.csv' not found. Exiting.")
-    exit(0)
+    print(f"Yesterday's file 'csvs/daily/{yesterday}.csv' not found. Proceeding without attachment.")
+    attach_file = False
+    body_content = f"The staking server is up, but has no attachment for today, {today}."
 
-encoded_file = base64.b64encode(data).decode()
-
-attachedFile = Attachment(
-    FileContent(encoded_file),
-    FileName(f'{yesterday}.csv'),
-    FileType('.csv'),
-    Disposition('attachment')
+message = Mail(
+    from_email=to_email,
+    to_emails=to_email,
+    subject="Staker status is UP.",
+    html_content=body_content
 )
-message.attachment = attachedFile
+
+if attach_file:
+    encoded_file = base64.b64encode(data).decode()
+
+    attachedFile = Attachment(
+        FileContent(encoded_file),
+        FileName(f'{yesterday}.csv'),
+        FileType('.csv'),
+        Disposition('attachment')
+    )
+    message.attachment = attachedFile
 
 try:
     response = sg.send(message)
